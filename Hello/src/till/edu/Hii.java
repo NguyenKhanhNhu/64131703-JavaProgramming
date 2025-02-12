e488c37a9bb6ff370ab787519bfd8a8cbef57837
Như Khánh <sanakuti26@gmail.com>
2025-02-12 16:37:37 +0700

    Ví dụ đầu tiên
    Chỉ để demo

---
+++ Hello/src/till/edu/Hii.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package till.edu;
+
+/**
+ * 
+ */
+public class Hii {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("À nhonnnn ");
+	}
+
+}
