378c0e2d311976d20470ad57bbdfcd641fadfcd3
Như Khánh <sanakuti26@gmail.com>
2025-02-17 10:28:31 +0700

    4
    nhập

--- Lab1_Bai4_Delta/src/delta.java
+++ Lab1_Bai4_Delta/src/delta.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
 public class delta {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Nhập a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Nhập b: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Nhập c: ");
+        double c = scanner.nextDouble();
+        
+	}
 }
