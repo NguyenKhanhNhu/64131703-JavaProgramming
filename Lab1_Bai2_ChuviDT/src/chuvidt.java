005df85a8ca3e6c1342407d2aa48b786f6ddbae9
Như Khánh <sanakuti26@gmail.com>
2025-02-17 10:07:19 +0700

    b2.2
    thíu cạnh nhỏ

--- Lab1_Bai2_ChuviDT/src/chuvidt.java
+++ Lab1_Bai2_ChuviDT/src/chuvidt.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
 
 public class chuvidt {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Nhập chiều dài: ");
+        double chieuDai = scanner.nextDouble();
+        
+        System.out.print("Nhập chiều rộng: ");
+        double chieuRong = scanner.nextDouble();
+        
+        double chuVi = 2 * (chieuDai + chieuRong);
+        double dienTich = chieuDai * chieuRong;
 
+        System.out.println("Chu vi: " + chuVi);
+        System.out.println("Diện tích: " + dienTich);
+        //System.out.println("Cạnh nhỏ nhất: " + );
+
+        scanner.close();
+
+	}
 }
