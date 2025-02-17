b862c60a5eb8439b971af3734e4ecbe9f58cc6d7
Như Khánh <sanakuti26@gmail.com>
2025-02-17 10:18:28 +0700

    3
    nhap canh

--- Lab1_Bai3_TheTich/src/thetich.java
+++ Lab1_Bai3_TheTich/src/thetich.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
 
 public class thetich {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Nhập cạnh của khối lập phương: ");
+        double canh = scanner.nextDouble();
+        
+        scanner.close();
+	}
 }
