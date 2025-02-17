3ac6464c9882a3f488fedc408b03a03e5ee71ef0
Như Khánh <sanakuti26@gmail.com>
2025-02-17 10:25:15 +0700

    3
    xoq

--- Lab1_Bai3_TheTich/src/thetich.java
+++ Lab1_Bai3_TheTich/src/thetich.java
@@ -7,6 +7,10 @@ public class thetich {
         System.out.print("Nhập cạnh của khối lập phương: ");
         double canh = scanner.nextDouble();
         
+        double theTich = Math.pow(canh, 3);
+
+        System.out.println("Thể tích khối lập phương: " + theTich);
+        
         scanner.close();
 	}
 }
